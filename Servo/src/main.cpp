d110be32129c788a4762e1d39e8857c4847516eb
leonbl <leon.blazic@gmail.com>
2022-12-14 08:04:51 +0100

    Update main.cpp

--- Servo/src/main.cpp
+++ Servo/src/main.cpp
@@ -1,9 +1,44 @@
 #include <Arduino.h>
+#define en1 11
+#define en2 3
+#define dir_clk 4
+#define dir_en 7
+#define dir_ser 8
+#define dir_latch 12
 
-void setup() {
-  // put your setup code here, to run once:
+void setStepCmd(uint8_t data);
+uint8_t ms=10;
+
+void setup()
+{
+  pinMode(en1, OUTPUT);
+  pinMode(en2, OUTPUT);
+  pinMode(dir_clk, OUTPUT);
+  pinMode(dir_en, OUTPUT);
+  pinMode(dir_ser, OUTPUT);
+  pinMode(dir_latch, OUTPUT);
+  digitalWrite(en1, HIGH);
+  digitalWrite(en2, HIGH);
+  digitalWrite(dir_en, HIGH);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  setStepCmd(0x04);
+  delay(ms);
+  setStepCmd(0x08);
+  delay(ms);
+  setStepCmd(0x02);
+  delay(ms);
+  setStepCmd(0x10);
+  delay(ms);
+}
+
+void setStepCmd(uint8_t data)
+{
+  digitalWrite(dir_en, HIGH);
+  digitalWrite(dir_latch, LOW);
+  shiftOut(dir_ser, dir_clk, MSBFIRST, data);
+  digitalWrite(dir_latch, HIGH);
+  digitalWrite(dir_en, LOW);
 }
